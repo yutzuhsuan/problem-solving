df2cad865ce2dfd51db02a3ba3990010b71cbbdf
geniustanley <geniustanley@gmail.com>
2017-01-29 12:15:19 +0800

    modified 11572 - Unique Snowflakes

--- UVa/11572 - Unique Snowflakes.cpp
+++ UVa/11572 - Unique Snowflakes.cpp
@@ -2,28 +2,37 @@
 #include <map>
 using namespace std;
 
+int num[1000005];
 int main(void)
 {
-  int t, n, snow, max;
+  int t, n, snow, max, cnt, pre;
   scanf("%d", &t);
 
   while (t--) {
     scanf("%d", &n);
     map<int, int> m;
     max = 0;
-    while (n--) {
-      scanf("%d", &snow);
-      if (m[snow]) {
-        if (m.size() > max)
-          max = m.size();
-        m.clear();
-      } else {
-        m[snow] = 1;
+    cnt = 0;
+    pre = 0;
+
+    for (int i = 1; i <= n; i++) {
+      scanf("%d", &num[i]);
+
+      if (m[num[i]] > pre) {
+        pre = m[num[i]];
       }
+
+      if (i - pre > max) {
+        max = i - pre;
+      }
+
+
+      // printf("%d %d %d\n", m[num[i]], max, pre);
+
+      m[num[i]] = i;
     }
 
     printf("%d\n", max);
-
   }
   return 0;
 }
